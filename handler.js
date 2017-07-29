2837154d4c314433191e954c31cde184d122e955
Akos Murati <akos.murati@xero.com>
2017-07-30 11:11:54 +1200

    Initial commit

---
+++ handler.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports.helloWorld = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+    },
+    body: JSON.stringify({
+      message: 'Is\' alive!!',
+      input: event,
+    }),
+  };
+
+  callback(null, response);
+};
